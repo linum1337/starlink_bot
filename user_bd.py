46941f1125ab1291e3e6b534ab02ba54b96f2c27
linum1337 <lebishova.elena@mail.ru>
2023-01-23 14:50:34 +0300

    Добавил выход из бота и сделал удобное изменение пути к бд для продакшн

--- user_bd.py
+++ user_bd.py
@@ -1,7 +1,7 @@
 import sqlite3
-
+bd_location = '/Users/vladislavcehov/PycharmProjects/starlink_bot/user_cookie.db'
 def user_add(id, cookie, login, pwd):
-    con = sqlite3.connect('/Users/vladislavcehov/PycharmProjects/starlink_bot/user_cookie.db')
+    con = sqlite3.connect(bd_location)
     cursor = con.cursor()
     sqlite_insertion = """INSERT INTO user_data (user_id, cookie, login, pwd) VALUES (?, ?, ?, ?)"""
     insertion_data = (str(id), str(cookie), str(login), str(pwd))
@@ -10,7 +10,7 @@ def user_add(id, cookie, login, pwd):
     cursor.close()
 
 def user_search(id):
-    con = sqlite3.connect('/Users/vladislavcehov/PycharmProjects/starlink_bot/user_cookie.db')
+    con = sqlite3.connect(bd_location)
     cursor = con.cursor()
     get_all = """SELECT * FROM user_data"""
     cursor.execute(get_all)
@@ -25,7 +25,7 @@ def user_search(id):
     return (False, 'fuck it', 1, 1)
 
 def delete_user(id):
-    con = sqlite3.connect('/Users/vladislavcehov/PycharmProjects/starlink_bot/user_cookie.db')
+    con = sqlite3.connect(bd_location)
     cursor = con.cursor()
     get_all = """DELETE FROM user_data WHERE user_id=(?)"""
     insertion = str(id)
