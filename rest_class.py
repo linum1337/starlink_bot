527e260843a587ff452c29f74ae6b999b18d7986
linum1337 <lebishova.elena@mail.ru>
2023-02-24 02:41:24 +0300

    fixed ip

--- rest_class.py
+++ rest_class.py
@@ -1,6 +1,8 @@
 
 
 import requests
+ip_out = '85.26.197.246:8082'
+ip_inner = '100.64.0.84:8082'
 def tg_id(abonent_id, tg):
     headers = {
         'Content-Type': 'application/x-www-form-urlencoded',
@@ -8,7 +10,7 @@ def tg_id(abonent_id, tg):
 
     data = 'method1=objects.create&arg1={"abonent_id":"' + str(abonent_id) + '","attribute_id":"1012","attribute_value":"'+ str(tg) + '"}'
 
-    response = requests.post('http://85.26.197.246:8082/rest_api/v2/AttributeValues/', headers=headers, data=data)
+    response = requests.post('http://' + ip_inner + ':8082:8082/rest_api/v2/AttributeValues/', headers=headers, data=data)
     print(response)
 
 
@@ -21,7 +23,7 @@ def abon_id(login):
 
     data = 'method1=objects.filter&arg1={"login":"' + login + '"}'
 
-    response = requests.post('http://85.26.197.246:8082/rest_api/v2/Users/', headers=headers, data=data).json()
+    response = requests.post('http://' + ip_inner + '/rest_api/v2/Users/', headers=headers, data=data).json()
     return response['result'][0]['fields']['abonent_id']
 def abon_balance(abon_id):
     headers = {
@@ -30,7 +32,7 @@ def abon_balance(abon_id):
 
     data = 'method1=objects.get&arg1={"account_id_abonents":"' + str(abon_id) + '"}&fields=["balance"]'
 
-    response = requests.post('http://85.26.197.246:8082/rest_api/v2/AdminAccounts/', headers=headers, data=data).json()
+    response = requests.post('http://' + ip_inner + '/rest_api/v2/AdminAccounts/', headers=headers, data=data).json()
     return response['result']['fields']['balance']
 def abon_tarif(abon_id):
     headers = {
@@ -39,10 +41,10 @@ def abon_tarif(abon_id):
 
     data = 'method1=objects.get&arg1={"id":"' + str(abon_id) + '"}&fields=["tarif_id"]'
 
-    response = requests.post('http://85.26.197.246:8082/rest_api/v2/Abonents/', headers=headers, data=data).json()
+    response = requests.post('http://' + ip_inner + '/rest_api/v2/Abonents/', headers=headers, data=data).json()
     tarif_id = str(response['result']['fields']['tarif_id'])
     data_for_tarif = 'method1=objects.get&arg1={"id":"' + str(tarif_id) + '"}&fields=["name"]'
-    response_for_tarif = requests.post('http://85.26.197.246:8082/rest_api/v2/Tarif/', headers=headers, data=data_for_tarif).json()
+    response_for_tarif = requests.post('http://' + ip_inner + '/rest_api/v2/Tarif/', headers=headers, data=data_for_tarif).json()
     return response_for_tarif['result']['fields']['name']
 
 def abon_usluga(abon_id):
@@ -52,7 +54,7 @@ def abon_usluga(abon_id):
 
     data = 'method1=objects.filter&arg1={"abonent_id":' + str(abon_id) + '}&fields=["usluga__name","usluga__comments","comment"]'
 
-    response = requests.post('http://85.26.197.246:8082/rest_api/v2/UsersUsluga/', headers=headers, data=data).json()
+    response = requests.post('http://' + ip_inner + '/rest_api/v2/UsersUsluga/', headers=headers, data=data).json()
     uslugi = []
     response = response['result']
     for i in range(0, len(response)):
