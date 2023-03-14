6041585694d4662c544ef1cb211de036d3ee8b9a
linum1337 <lebishova.elena@mail.ru>
2023-03-14 17:55:01 +0300

    внес изменения в генератор qr кода

--- qr_generator.py
+++ qr_generator.py
@@ -3,14 +3,12 @@ import parser_selenium
 import rest_class
 from datetime import datetime
 def qr_generator(data):
+
        abonent_id = rest_class.abon_id(data[3])
        client = rest_class.abon_street(abonent_id)
-       metadata = datetime.now().date()
-       print(metadata)
-       data = f"Дата создания: {metadata} \n"  \
-              f"Договор: {data[3]} \n" \
-              f"Абонент: \n {client}"
-
+       metadata = datetime.now()
+       dt_string = metadata.strftime("%d/%m/%Y %H:%M")
+       data = f"{dt_string} \n"  \
+              f"{data[3]} \n"
        img = qrcode.make(data)
        img.save("test1.png")
-
